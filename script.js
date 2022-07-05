c15f123efaf8ded54106878c4975cdf20ce5eec0
CJskii <czareczarek3@gmail.com>
2022-07-05 22:27:48 +0100

    created playRound function

--- script.js
+++ script.js
@@ -3,31 +3,41 @@ let playerSelection = window.prompt("Input Rock, Paper or Scissors: ");
 playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 const computerSelection = computerPlay();
 
-alert("Your selection is " + playerSelection);
+
 
 function computerPlay() {
     return myArray[Math.floor(Math.random() * myArray.length)]
 }
 
+alert("Your selection is " + playerSelection);
+
+//console.log(computerSelection);
 
 
-console.log(computerSelection);
+function playRound(playerSelection, computerSelection){
+    console.log("Your choice: " + playerSelection)
+    console.log("Computer choice: " + computerSelection);
+    if (playerSelection == "Rock" && computerSelection == "Scissors"){
+        console.log("You won. Rock beats Scissors");
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+        console.log("You won. Paper beats Scissors");
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+        console.log("You won. Scissors beats Paper");
+    } else if (playerSelection === computerSelection){
+        console.log("Draw. Try again");
+    } else {
+        console.log("You lost... Better luck next time");
+    }
+}
+
+playRound(playerSelection, computerSelection);
+
 
 /*
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "scissors"){
-        return "Rock beats Scissors";
-    } else if (playerSelection == "paper" && computerSelection == "scissors"){
-        return "Peper beats Scissors";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "Scissors beats Paper";
-    }
-  }
+console.log(playRound(playerSelection, computerSelection));
 
-console.log(playRound(Rock, Scissors));
-
-function game(){
+ function game(){
 
 }
 
