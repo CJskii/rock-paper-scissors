b563f11fa1c1c05b5db34631e3217b1aa4a11675
CJskii <czareczarek3@gmail.com>
2022-07-05 23:01:11 +0100

    changed playRound outputs and added comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const myArray = ['Rock', 'Paper', 'Scissors'];
-let playerSelection = window.prompt("Input Rock, Paper or Scissors: ");
+let playerSelection = window.prompt("Input Rock, Paper or Scissors: "); // prompts user for input
 playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 const computerSelection = computerPlay();
 
@@ -18,19 +18,35 @@ function playRound(playerSelection, computerSelection){
     console.log("Your choice: " + playerSelection)
     console.log("Computer choice: " + computerSelection);
     if (playerSelection == "Rock" && computerSelection == "Scissors"){
-        console.log("You won! Rock beats Scissors");
+        console.log("You won! Rock beats Scissors")
+        let winner = "Won"
+        alert("Game " + winner);
+        return winner;
     } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
         console.log("You won! Paper beats Scissors");
+        let winner = "Won"
+        alert("Game " + winner);
+        return winner;
     } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
         console.log("You won! Scissors beats Paper");
+        let winner = "Won"
+        alert("Game " + winner);
+        return winner;
     } else if (playerSelection === computerSelection){
         console.log("Draw. Try again");
+        let winner = "Draw"
+        alert("Game " + winner);
+        return winner;
     } else {
         console.log("You lose! Better luck next time");
+        let winner = "Lost"
+        alert("Game " + winner);
+        return winner;
     }
 }
 
-playRound(playerSelection, computerSelection);
+
+playRound(playerSelection, computerSelection); // plays game and prints output to console
 
 
 /*
