1ed42bb575fbb0ac4b6304c907c6926c06e37e2b
CJskii <czareczarek3@gmail.com>
2022-07-06 14:52:08 +0100

    cleaned code from unnecessary comments

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const myArray = ['Rock', 'Paper', 'Scissors'];
-let playerSelection = 0 //window.prompt("Input Rock, Paper or Scissors: "); // prompts user for input
-//playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+let playerSelection = 0 
 const computerSelection = computerPlay();
 let winner = 0;
 let point = 0;
@@ -8,10 +7,6 @@ let point = 0;
 function computerPlay() {
     return myArray[Math.floor(Math.random() * myArray.length)]
 }
-
-//alert("Your selection is " + playerSelection);
-
-
 
 function playRound(playerSelection, computerSelection){
     let player = playerSelection
@@ -23,65 +18,36 @@ function playRound(playerSelection, computerSelection){
     if (player == "Rock" && computer == "Scissors"){
         console.log("***You won! Rock beats Scissors")
         let winner = "Won"
-        //console.log(winner);
         return winner;
     } else if (player == "Paper" && computer == "Rock"){
         console.log("***You won! Paper beats Scissors"); // prints in the console
         let winner = "Won"
-        //console.log(winner);
         return winner;
     } else if (player == "Scissors" && computer == "Paper"){
         console.log("***You won! Scissors beats Paper"); // prints in the console
         let winner = "Won"
-        //console.log(winner);
         return winner;
     } else if (player === computer){
         console.log("***Draw. Try again"); // prints in the console
         let winner = "Draw"
-        //console.log(winner);
         return winner;
     } else {
         console.log("***You lose! Better luck next time"); // prints in the console
         let winner = "Lost"
-        //console.log(winner);
         return winner;
     }
 }
-
-
-//winner = playRound();
-//console.log(winner)
-
-//playRound(playerSelection, computerSelection); // plays game and prints output to console
-
-
 
  function game(){
     let point = 0;
     for (let i = 0; i < 5; i++ ){
         let winner = playRound();
-        //let player = window.prompt("Input Rock, Paper or Scissors: "); // prompts user for input
-        //player = player.charAt(0).toUpperCase() + player.slice(1).toLowerCase();
-        //console.log("*Your choice: " + winner) // prints in the console
-        //console.log("**Computer choice: " + computer); // prints in the console
         if (winner == "Won"){
-            //output = playRound(playerSelection, computerSelection)
-            //console.log("***You won! Congratulations"); // prints in the console
-            //console.log(output);
             point++
-            //console.log(point)
         } else if (winner == "Lost"){
-            //output = playRound(playerSelection, computerSelection)
-            //console.log("***You lose! Better luck next time"); // prints in the console
-            //console.log(output)
             point--
-            //console.log(point)
         } else if (winner == "Draw") {
-            //output = playRound(playerSelection, computerSelection)
-            //console.log("***Draw. Try again"); // prints in the console
-            //console.log(output)
             point
-            //console.log(point)
         }
     }   //console.log(point);
     let outcome
@@ -102,14 +68,3 @@ game();
 //playRound()
 
 
-
-/* Player Inputs a String 
-Player click "play"
-Computer play returns random array
-Function to compare inputs
-Print Rock beats scissors etc.
-*/
-
-
-
-//console.log(computerPlay("Paper"));
