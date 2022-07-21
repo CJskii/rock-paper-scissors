c514d79ba25da276a090bf3436f4b3d4e47d3403
CJskii <czareczarek3@gmail.com>
2022-07-21 18:03:33 +0100

    print five function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ let playerSelection = 0
 const computerSelection = computerPlay();
 let winner = 0;
 let point = 0;
-
+let playerPoint = 0;
+let computerPoint = 0;
 
 function computerPlay() {
     return myArray[Math.floor(Math.random() * myArray.length)]
@@ -14,12 +15,12 @@ function playerPrint(){ // function to print player selection in document
     const output = document.querySelector('#output') // target output div
     if (document.getElementById('#player') != null){
         document.getElementById('#player').remove();
-    }    
+    } 
     const playerPrint = document.createElement('p')
     playerPrint.setAttribute('id', '#player')
     playerPrint.setAttribute('class', 'h5 p-1')
     playerPrint.setAttribute('data-aos', 'fade-left')
-    console.log(this.output)
+    //console.log(this.output)
     playerPrint.textContent = `Your choice: ${player}`
     output.appendChild(playerPrint);
     return player;
@@ -30,7 +31,7 @@ function computerPrint(){ // function to print computer selection in document
     const output = document.querySelector('#output')
     if (document.getElementById('#computer') != null){
         document.getElementById('#computer').remove();
-    }    
+    } 
     const computerPrint = document.createElement('p')
     computerPrint.setAttribute('id', '#computer')
     computerPrint.setAttribute('class', 'h5 p-1')
@@ -57,42 +58,78 @@ function winnerPrint(){ // function to print winner outcome in document
 }
 
 
-function playRound(playerSelection, computerSelection){
-    
+function playRound(playerSelection, computerSelection){    
     player = playerPrint();
     computer = computerPrint();
-
-    console.log("*Your choice: " + player) // prints in the console
-    console.log("**Computer choice: " + computer); // prints in the console
+    
     if (player == "Rock" && computer == "Scissors"){
         winner = "You won! Rock beats Scissors"
         winnerPrint()
+        playerPoint++
+        fiveRounds()
         return winner;
     } else if (player == "Paper" && computer == "Rock"){
         winner = "You won! Paper beats Scissors"
+        playerPoint++
         winnerPrint()
+        fiveRounds()
         return winner;
     } else if (player == "Scissors" && computer == "Paper"){
         winner = "You won! Scissors beats Paper"
+        playerPoint++
         winnerPrint()
+        fiveRounds()
         return winner;
     } else if (player === computer){
         winner = "Draw. Try again"
         winnerPrint()
+        fiveRounds()
         return winner;
     } else {
         winner = "You lose! Better luck next time"
+        computerPoint++
+        console.log(computerPoint)
+        fiveRounds()
         winnerPrint()
         return winner;
     }
 }
 
 
-
-
+function fiveRounds(){
+    if (playerPoint == 5){
+        const output = document.querySelector('#output')
+        const fivePrint = document.createElement('p')
+        if (document.getElementById('#player') != null){
+            document.getElementById('#player').remove()
+            document.getElementById('#computer').remove()
+            document.getElementById('#outcome').remove()
+        }        
+        fivePrint.textContent = "You have won!" 
+        fivePrint.setAttribute('id', 'winner-5')
+        fivePrint.setAttribute('class', 'h1 text-center p-4')
+        output.appendChild(fivePrint)
+        playerPoint = 0;
+        return;
+    } else if (computerPoint == 5){
+        const output = document.querySelector('#output')
+        const fivePrint = document.createElement('p')
+        if (document.getElementById('#computer') != null){
+            document.getElementById('#player').remove()
+            document.getElementById('#computer').remove()
+            document.getElementById('#outcome').remove()
+        }
+        fivePrint.textContent = "You have Lost!" 
+        fivePrint.setAttribute('id', 'winner-5')
+        fivePrint.setAttribute('class', 'h1 text-center p-4')
+        output.appendChild(fivePrint);
+        computerPoint = 0;
+        return;
+    }
+}
 
 function logText(e){
-    console.log(this.textContent)
+    //console.log(this.textContent)
     playerSelection = this.textContent;
     return playRound(playerSelection);
 }
@@ -111,7 +148,7 @@ buttons.forEach(button => button.addEventListener('click', logText, {
 //playRound()
 
 
-/*function game(){
+function game(){
     let point = 0;
     for (let i = 0; i < 5; i++ ){
         let winner = playRound();
@@ -134,4 +171,4 @@ buttons.forEach(button => button.addEventListener('click', logText, {
         outcome = "Draw. Try again!"
         console.log("Final outcome of 5 games: " + outcome);
     } 
-}*/
+}
