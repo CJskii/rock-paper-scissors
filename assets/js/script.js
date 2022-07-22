9114f86ec03f1e56f0375a5a37140ae38a8912c8
CJskii <czareczarek3@gmail.com>
2022-07-22 19:54:07 +0100

    UI completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,28 @@
 const myArray = ['Rock', 'Paper', 'Scissors'];
 let playerSelection = 0 
-const computerSelection = computerPlay();
 let winner = 0;
-let point = 0;
 let playerPoint = 0;
 let computerPoint = 0;
 
-function computerPlay() {
-    return myArray[Math.floor(Math.random() * myArray.length)]
+const buttons = document.querySelectorAll('button'); 
+//button listener
+buttons.forEach(button => button.addEventListener('click', logText, {
+    capture:true
+}))
+
+function logText(e){
+    playerSelection = this.textContent;
+    return playRound(playerSelection); // calls playRound()
 }
 
-function playerPrint(){ // function to print player selection in document
-    let player = playerSelection
-    const output = document.querySelector('#output') // target output div
-    if (document.getElementById('#player') != null){
-        document.getElementById('#player').remove();
-    } 
-    const playerPrint = document.createElement('p')
-    playerPrint.setAttribute('id', '#player')
-    playerPrint.setAttribute('class', 'h5 p-1')
-    playerPrint.setAttribute('data-aos', 'fade-left')
-    //console.log(this.output)
-    playerPrint.textContent = `Your choice: ${player}`
-    output.appendChild(playerPrint);
-    return player;
-}
-
-function computerPrint(){ // function to print computer selection in document
-    let computer = computerPlay()
-    const output = document.querySelector('#output')
-    if (document.getElementById('#computer') != null){
-        document.getElementById('#computer').remove();
-    } 
-    const computerPrint = document.createElement('p')
-    computerPrint.setAttribute('id', '#computer')
-    computerPrint.setAttribute('class', 'h5 p-1')
-    computerPrint.setAttribute('data-aos', 'fade-right')
-    computerPrint.setAttribute('data-aos-delay', '300')
-    computerPrint.textContent = `Computer choice: ${computer}`
-    output.appendChild(computerPrint);
-    return computer;
-}
-
-function winnerPrint(){ // function to print winner outcome in document
-    const output = document.querySelector('#output')
-    const winnerPrint = document.createElement('p')
-    if (document.getElementById('#outcome') != null){
-        document.getElementById('#outcome').remove()
-    }
-    winnerPrint.setAttribute('id', '#outcome')
-    winnerPrint.setAttribute('class', 'h4 p-1')
-    winnerPrint.setAttribute('data-aos', 'fade-up')
-    winnerPrint.setAttribute('data-aos-delay', '600')
-    winnerPrint.textContent = `${winner}`
-    output.appendChild(winnerPrint)
-    return winner;
-}
-
-
-function playRound(playerSelection, computerSelection){    
-    player = playerPrint();
+function playRound(playerSelection){  // function to play one round
+    removeText(); 
+    player = playerPrint(); 
     computer = computerPrint();
-    
     if (player == "Rock" && computer == "Scissors"){
         winner = "You won! Rock beats Scissors"
-        winnerPrint()
         playerPoint++
+        winnerPrint()        
         fiveRounds()
         return winner;
     } else if (player == "Paper" && computer == "Rock"){
@@ -86,89 +43,87 @@ function playRound(playerSelection, computerSelection){
         fiveRounds()
         return winner;
     } else {
-        winner = "You lose! Better luck next time"
+        winner = "You lost! Better luck next time"
         computerPoint++
-        console.log(computerPoint)
-        fiveRounds()
         winnerPrint()
+        fiveRounds()
         return winner;
     }
 }
 
+function removeText(){ // function loop through paragraphs and remove 
+    const paragraphs = document.querySelectorAll('p')
+        paragraphs.forEach(function(paragraph){
+        if (paragraph.textContent) { // remove if paragraph has text content
+        paragraph.remove()
+    }
+        });
+}
 
-function fiveRounds(){
-    if (playerPoint == 5){
+function playerPrint(){ // function to print player selection in document
+    let player = playerSelection
+    const output = document.querySelector('#output') 
+    const playerPrint = document.createElement('p')
+    playerPrint.setAttribute('id', '#player')
+    playerPrint.setAttribute('class', 'h5 p-1')
+    playerPrint.setAttribute('data-aos', 'fade-left')
+    playerPrint.textContent = `Your choice: ${player}`
+    output.appendChild(playerPrint);
+    return player;
+}
+
+function computerPrint(){ // function to print computer selection in document
+    let computer = computerPlay()
+    const output = document.querySelector('#output')
+    const computerPrint = document.createElement('p')
+    computerPrint.setAttribute('id', '#computer')
+    computerPrint.setAttribute('class', 'h5 p-1')
+    computerPrint.setAttribute('data-aos', 'fade-right')
+    computerPrint.setAttribute('data-aos-delay', '300')
+    computerPrint.textContent = `Computer choice: ${computer}`
+    output.appendChild(computerPrint);
+    return computer;
+}
+
+function computerPlay() { // function for computerSelection
+    return myArray[Math.floor(Math.random() * myArray.length)]
+}
+
+function winnerPrint(){ // function to print winner outcome in document
+    const output = document.querySelector('#output')
+    const winnerPrint = document.createElement('p')
+    winnerPrint.setAttribute('id', '#outcome')
+    winnerPrint.setAttribute('class', 'h4 p-1')
+    winnerPrint.setAttribute('data-aos', 'fade-up')
+    winnerPrint.setAttribute('data-aos-delay', '600')
+    winnerPrint.textContent = `${winner}`
+    output.appendChild(winnerPrint)
+    return winner;
+}
+
+function fiveRounds(){ // function to print winner of five rounds
+    if (playerPoint == 5){ // points condition
+        removeText()
         const output = document.querySelector('#output')
-        const fivePrint = document.createElement('p')
-        if (document.getElementById('#player') != null){
-            document.getElementById('#player').remove()
-            document.getElementById('#computer').remove()
-            document.getElementById('#outcome').remove()
-        }        
+        const fivePrint = document.createElement('p')    
         fivePrint.textContent = "You have won!" 
         fivePrint.setAttribute('id', 'winner-5')
         fivePrint.setAttribute('class', 'h1 text-center p-4')
         output.appendChild(fivePrint)
         playerPoint = 0;
+        computerPoint = 0;
         return;
-    } else if (computerPoint == 5){
+    } else if (computerPoint == 5){ // points condition
+        removeText()
         const output = document.querySelector('#output')
         const fivePrint = document.createElement('p')
-        if (document.getElementById('#computer') != null){
-            document.getElementById('#player').remove()
-            document.getElementById('#computer').remove()
-            document.getElementById('#outcome').remove()
-        }
         fivePrint.textContent = "You have Lost!" 
         fivePrint.setAttribute('id', 'winner-5')
         fivePrint.setAttribute('class', 'h1 text-center p-4')
         output.appendChild(fivePrint);
         computerPoint = 0;
+        playerPoint = 0;
         return;
     }
 }
 
-function logText(e){
-    //console.log(this.textContent)
-    playerSelection = this.textContent;
-    return playRound(playerSelection);
-}
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', logText, {
-    capture:true
-}))
-
-
-
-
-
-//game(); 
-
-//playRound()
-
-
-function game(){
-    let point = 0;
-    for (let i = 0; i < 5; i++ ){
-        let winner = playRound();
-        if (winner == "Won"){
-            point++
-        } else if (winner == "Lost"){
-            point--
-        } else if (winner == "Draw") {
-            point
-        }
-    }   //console.log(point);
-    let outcome
-    if (point > 0){
-        outcome = "You won!"
-        console.log("Final outcome of 5 games: " + outcome);
-    } else if (point < 0){
-        outcome = "You lost!"
-        console.log("Final outcome of 5 games: " + outcome);
-    } else {
-        outcome = "Draw. Try again!"
-        console.log("Final outcome of 5 games: " + outcome);
-    } 
-}
